88b7924d3d6b0a72a328e7cfc2e1051ffb33aaeb
root <root@ip-172-31-11-31.ap-northeast-1.compute.internal>
2023-03-29 23:26:13 +0000

    check file

---
+++ momtechnologies/src/test/java/1/0/AppTest.java
@@ -0,0 +1,20 @@
+package 1.0;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
